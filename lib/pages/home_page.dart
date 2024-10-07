d858b5575b61560194b4ad7e8a146e784b456ac9
William Pettersen <williamkpettersen@outlook.com>
2024-10-08 00:39:59 +1100

    added drawer and list tiles

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.grey[100],
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(
+              Icons.favorite,
+              size: 48,
+              )
+            ),
+
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("H O M E")
+            ),
+
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("S E T T I N G S")
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
